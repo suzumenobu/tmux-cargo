c8e8d860356facc7ee715ce244bfa0f4958eae12
idevtier <idevtier@gmail.com>
2022-11-20 21:29:06 +0300

    Adding new commands

--- scripts/run_cargo.sh
+++ scripts/run_cargo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-commands=`echo "test select_test run clippy build custom help" | tr ' ' '\n'`
+commands=`echo "build check clean doc new init add run test select_test bench update publish" | tr ' ' '\n'`
 selected=`printf "$commands" | fzf`
 
 # Go to crate root
@@ -9,6 +9,26 @@ cd $1
 # Exit if nothing selected
 if [[ -z $selected ]]; then
     exit
+
+# Search crates
+elif [ $selected = "search" ]; then
+    read -p "Search: " query
+    cargo $selected $query
+
+# Install binary
+elif [ $selected = "install" ]; then
+    read -p "Install: " query
+    cargo $selected $query
+
+# Create new crate
+elif [ $selected = "new" ]; then
+    read -p "New: " query
+    cargo $selected $query
+
+# Add dependency
+elif [ $selected = "add" ]; then
+    read -p "Add: " query
+    cargo $selected $query
 
 # Parse all tests and do fzf
 elif [ $selected = "select_test" ]; then
@@ -22,6 +42,11 @@ elif [ $selected = "select_test" ]; then
         cargo test $test
     fi
 
+# Run all tests and enter copy mode
+elif [ $selected = "test" ]; then
+    cargo $selected
+    tmux copy
+
 # Read custom command
 elif [ $selected = "custom" ]; then
     read -p "Enter cargo arguments: " query
@@ -32,8 +57,8 @@ else
     cargo $selected
 fi
 
-# Enter tmux copy mode for scrolling
-tmux copy
+echo
+echo "COMPLETE"
 
 # Prevent window to close
 read
